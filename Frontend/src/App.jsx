fa82f7b79ce9d209971c4ec5c8c657ab12a4cb58
Manish kumar raj <167297152+manishraj25@users.noreply.github.com>
2025-10-19 00:10:37 +0530

    make changes in App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Home from './pages/Home'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
 
 function App() {
-
   return (
-    <Router>
-      <Home />
-    </Router>
-  )
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
